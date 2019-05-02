3631d26f2994d9a03d06ab329c465eaa2ca35f3e
Mattia <mattiagiacom3llo@gmail.com>
2019-05-02 16:15:43 +0200

    Sviluppo dell'applicazione front-end
    Creato il progetto Xcode

---
+++ What a test!/What a test!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  What a test!
+//
+//  Created by Mattia Mello on 02/05/2019.
+//  Copyright © 2019 Mattia Mello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ What a test!/operator.swift
@@ -0,0 +1,9 @@
+//
+//  operator.swift
+//  What a test!
+//
+//  Created by Mattia Mello on 02/05/2019.
+//  Copyright © 2019 Mattia Mello. All rights reserved.
+//
+
+import Foundation
